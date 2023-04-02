480cc30c43dd4dad398212a649428724f0ae26ae
Ankush Waghmare <waghmareankush050@gmail.com>
2023-04-02 23:14:01 +0530

    form making done

---
+++ src/components/Form.js
@@ -0,0 +1,44 @@
+import React, { useRef } from 'react'
+
+const Form = () => {
+
+    const nameRef = useRef('')
+    const descriptionRef = useRef('')
+    const priceRef = useRef('')
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+
+        const data ={
+            inputName:nameRef.current.value,
+            inputDescription:descriptionRef.current.value,
+            inputPrice:priceRef.current.value
+        }
+
+        console.log(data)
+
+        event.target.reset()
+
+    }
+
+  return (
+    <>
+      <form className='form' onSubmit={submitHandler}>
+
+        <label htmlFor="brandname"> Name : </label>
+        <input type="text" ref={nameRef} />
+        <label htmlFor="brandname"> Description : </label>
+        <input type="text" ref={descriptionRef} />
+        <label htmlFor="brandname"> Price : </label>
+        <input type="number" ref={priceRef} />
+        
+        <button>Add Product</button>
+
+
+
+      </form>
+    </>
+  )
+}
+
+export default Form
